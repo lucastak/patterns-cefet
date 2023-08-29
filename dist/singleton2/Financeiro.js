c145ebad73638228089f923438f9e666714eb581
Lucas Santos <lucassantos@MacBook-Air-de-Lucas.local>
2023-08-29 19:39:38 -0300

    feat: sobre exemplo do adapter

---
+++ dist/singleton2/Financeiro.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Financeiro = void 0;
+class Financeiro {
+    constructor(funcionarioNome, funcionarioSalario) {
+        this.nome = funcionarioNome;
+        this.salario = funcionarioSalario;
+    }
+}
+exports.Financeiro = Financeiro;
+//# sourceMappingURL=Financeiro.js.map
---
+++ readme.md
@@ -0,0 +1,2 @@
+para transformar ts para js - npx tsc arquivo
+para rodar o arquivo - node aquivo
